e1d23afc6b0e60c12b795294ffe3c36c54df2e4f
Phili0916 <79313259+Phili0916@users.noreply.github.com>
2024-02-13 13:54:47 +0100

    initial commit

---
+++ src/components/random-color/RandomColor.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const RandomColor = () => {
+    return (
+        <div className="container">
+            <button>Generate Color</button>
+        </div>
+    )
+}
+
+export default RandomColor
